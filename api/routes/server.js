da64a0bcc41b5590643583ec7ce38cd83ef106fc
Martijn <mdoodewa@avans.nl>
2021-06-22 14:38:40 +0200

    added videosign verification

--- api/routes/server.js
+++ api/routes/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const TruYouController = require('../controllers/videoSign');
 const serverController = require('../controllers/server');
 
 module.exports = (context) => {
   let router = express.Router();
   router.get('/', serverController.getInfo.bind(context));
+  router.put('/streamdata', TruYouController.update_stream_data);
   return router;
 };
